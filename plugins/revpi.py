70425466582c9b13d7362358913defda9f4fcf56
Ahmad Arabi Katbi <a.katbi@kunbus.com>
2024-08-19 11:38:34 +0200

    feat: Collect info of all files in `interfaces.d`
    Signed-off-by: Ahmad Arabi Katbi <a.katbi@kunbus.com>
    (cherry picked from commit fe9ca4e9accef3abbc8993f9c20cd5e829006080)
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- plugins/revpi.py
+++ plugins/revpi.py
@@ -35,6 +35,7 @@ class RevPi(Plugin, DebianPlugin):
                 "/etc/revpi/image-release",
                 "/etc/dhcpcd.conf",
                 "/etc/network/interfaces",
+                "/etc/network/interfaces.d/*",
                 "/etc/resolv.conf",
                 "/sys/class/net/*/carrier",
                 "/sys/class/net/*/speed",
